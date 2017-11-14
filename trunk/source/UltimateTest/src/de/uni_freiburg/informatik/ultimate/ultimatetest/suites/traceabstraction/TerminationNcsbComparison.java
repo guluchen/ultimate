12ecf48d2c3747546ab43f30e1102455ce9471ee
Yong Li <duckly_lee@163.com>
2017-11-14 10:40:22 +0800

    Update TerminationNcsbComparison.java

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimate/ultimatetest/suites/traceabstraction/TerminationNcsbComparison.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimate/ultimatetest/suites/traceabstraction/TerminationNcsbComparison.java
@@ -411,17 +411,20 @@ public class TerminationNcsbComparison extends AbstractBuchiAutomizerTestSuite {
 //			"buchiAutomizer/ncsb/ROSE-ORIGINAL.epf", //FA, NBA
 //			"buchiAutomizer/ncsb/DAISY-ORIGINAL.epf", //CAV 14
 		        // for the experiments
-		       // sunflower setting
-		       "buchiAutomizer/ncsb/SUNFLOWER_BA.epf",
-		       "buchiAutomizer/ncsb/SUNFLOWER_BA_LAZYS.epf",
-		       "buchiAutomizer/ncsb/SUNFLOWER_GBA_LAZYS.epf",
-		       "buchiAutomizer/ncsb/SUNFLOWER_GBA_LAZYS_ANTICHAIN.epf",
+		    "buchiAutomizer/ncsb/SUNFLOWER_BA.epf",
+		    "buchiAutomizer/ncsb/SUNFLOWER_BA_LAZYS.epf",
+		    "buchiAutomizer/ncsb/SUNFLOWER_GBA_ANTICHAIN.epf",
+		    "buchiAutomizer/ncsb/SUNFLOWER_GBA.epf",
+		    "buchiAutomizer/ncsb/SUNFLOWER_GBA_LAZYS.epf",
+		    "buchiAutomizer/ncsb/SUNFLOWER_GBA_LAZYS_ANTICHAIN.epf",
 		
 		       // svcomp setting
-		       "buchiAutomizer/ncsb/RHODODENDRON_BA.epf",
-		       "buchiAutomizer/ncsb/RHODODENDRON_BA_LAZYS.epf",
-		       "buchiAutomizer/ncsb/RHODODENDRON_GBA_LAZYS.epf",
-		       "buchiAutomizer/ncsb/RHODODENDRON_GBA_LAZYS_ANTICHAIN.epf",
+		    "buchiAutomizer/ncsb/RHODODENDRON_BA.epf",
+		    "buchiAutomizer/ncsb/RHODODENDRON_BA_LAZYS.epf",
+		    "buchiAutomizer/ncsb/RHODODENDRON_GBA_ANTICHAIN.epf",
+		    "buchiAutomizer/ncsb/RHODODENDRON_GBA.epf",
+		    "buchiAutomizer/ncsb/RHODODENDRON_GBA_LAZYS.epf",
+		    "buchiAutomizer/ncsb/RHODODENDRON_GBA_LAZYS_ANTICHAIN.epf",
 		
 	};
 
